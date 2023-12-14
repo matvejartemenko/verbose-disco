57002703bf87670c9ec400619dc8e6aa06a52df4
Mike <152637381+matvejartemenko@users.noreply.github.com>
2023-12-15 01:33:33 +0530

    Create verbose-disco.swift

---
+++ verbose-disco.swift
@@ -0,0 +1,8 @@
+case .disconnectControlPoints(let ids):
+            newCollage = disconnectControlPoints(ids, in: newCollage)
+            
+        case .changeCornerRadius(let radius):
+            newCollage.cornerRadius = radius
+case .scale(let gestureState):
+            newState = onSale(gestureState, in: newState)
+            
